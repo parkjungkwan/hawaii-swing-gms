f8d63fc9481990add05a0511bd85036f999c9124
pakjkwan <pakjkwan@gmail.com>
2020-04-13 18:02:46 +0900

    18:02

--- member/JoinView.java
+++ member/JoinView.java
@@ -8,6 +8,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class JoinView extends JFrame implements ActionListener{
@@ -15,6 +16,8 @@ public class JoinView extends JFrame implements ActionListener{
 	JButton saveButton, listButton, loginButton;
 	JLabel[] labels;
 	JTextField[] textFields;
+	JLabel areaLabel;
+	JTextArea textArea;
 	JPanel panel;
 	MemberService memberService;
 	public JoinView() {
@@ -27,15 +30,19 @@ public class JoinView extends JFrame implements ActionListener{
 		panel = new JPanel();
 		this.add(panel);
 		this.setVisible(true);
-		labels = new JLabel[6];
-		textFields = new JTextField[6];
-		String[] names = {"이름","아이디","패스워드","SSN","주소","검색결과"};
+		labels = new JLabel[5];
+		textFields = new JTextField[5];
+		textArea = new JTextArea();
+		String[] names = {"이름","아이디","패스워드","SSN","주소"};
 		for(int i=0;i < names.length;i++) {
 			labels[i] = new JLabel(names[i]);
 			panel.add(labels[i]);
 			textFields[i] = new JTextField();
 			panel.add(textFields[i]);
 		}	
+		areaLabel = new JLabel("검색결과");
+		panel.add(areaLabel);
+		panel.add(textArea);
 		saveButton = new JButton("저장");
 		listButton = new JButton("목록");
 		loginButton = new JButton("로그인");
@@ -49,13 +56,13 @@ public class JoinView extends JFrame implements ActionListener{
 		labels[2].setBounds(40,90,60,40);
 		labels[3].setBounds(40,130,40,40);
 		labels[4].setBounds(40,170,60,40);
-		labels[5].setBounds(40,210,60,40);
+		areaLabel.setBounds(40,210,60,40);
 		textFields[0].setBounds(120,10,200,30);
 		textFields[1].setBounds(120,50,200,30);
 		textFields[2].setBounds(120,90,200,30);
 		textFields[3].setBounds(120,130,280,30);
 		textFields[4].setBounds(120,180,280,30);
-		textFields[5].setBounds(120,220,280,150);
+		textArea.setBounds(120,220,280,150);
 		saveButton.setBounds(125,400,80,30);
 		listButton.setBounds(240,400,80,30);
 		loginButton.setBounds(340,400,80,30);
@@ -95,6 +102,9 @@ public class JoinView extends JFrame implements ActionListener{
 			}
 			
 		}else if(e.getSource() == listButton) {
+			JOptionPane.showMessageDialog(this, "클릭 !!");
+			
+			
 			
 		}
 		
