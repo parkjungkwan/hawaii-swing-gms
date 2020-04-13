e47637210f782c7ff5237a1de9454665c127fe18
pakjkwan <pakjkwan@gmail.com>
2020-04-13 17:33:44 +0900

    17:33

--- member/JoinView.java
+++ member/JoinView.java
@@ -12,7 +12,7 @@ import javax.swing.JTextField;
 
 public class JoinView extends JFrame implements ActionListener{
 	private static final long serialVersionUID = 1L;
-	JButton saveButton, cancelButton;
+	JButton saveButton, listButton, loginButton;
 	JLabel[] labels;
 	JTextField[] textFields;
 	JPanel panel;
@@ -22,14 +22,14 @@ public class JoinView extends JFrame implements ActionListener{
 	}
 	public void open() {
 		
-		this.setSize(500, 500);
+		this.setSize(600, 600);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		panel = new JPanel();
 		this.add(panel);
 		this.setVisible(true);
-		labels = new JLabel[5];
-		textFields = new JTextField[5];
-		String[] names = {"이름","아이디","패스워드","SSN","주소"};
+		labels = new JLabel[6];
+		textFields = new JTextField[6];
+		String[] names = {"이름","아이디","패스워드","SSN","주소","검색결과"};
 		for(int i=0;i < names.length;i++) {
 			labels[i] = new JLabel(names[i]);
 			panel.add(labels[i]);
@@ -37,23 +37,28 @@ public class JoinView extends JFrame implements ActionListener{
 			panel.add(textFields[i]);
 		}	
 		saveButton = new JButton("저장");
-		cancelButton = new JButton("취소");
+		listButton = new JButton("목록");
+		loginButton = new JButton("로그인");
 		saveButton.addActionListener(this);
-		cancelButton.addActionListener(this);
+		listButton.addActionListener(this);
 		panel.add(saveButton); 
-		panel.add(cancelButton);
+		panel.add(listButton);
+		panel.add(loginButton);
 		labels[0].setBounds(40,10,40,40);
 		labels[1].setBounds(40,50,40,40);
 		labels[2].setBounds(40,90,60,40);
 		labels[3].setBounds(40,130,40,40);
 		labels[4].setBounds(40,170,60,40);
+		labels[5].setBounds(40,210,60,40);
 		textFields[0].setBounds(120,10,200,30);
 		textFields[1].setBounds(120,50,200,30);
 		textFields[2].setBounds(120,90,200,30);
 		textFields[3].setBounds(120,130,280,30);
-		textFields[4].setBounds(120,180,280,120);
-		saveButton.setBounds(125,330,80,30);
-		cancelButton.setBounds(240,330,80,30);
+		textFields[4].setBounds(120,180,280,30);
+		textFields[5].setBounds(120,220,280,150);
+		saveButton.setBounds(125,400,80,30);
+		listButton.setBounds(240,400,80,30);
+		loginButton.setBounds(340,400,80,30);
 		
 		this.setLocationRelativeTo(null);
 	}
@@ -89,7 +94,7 @@ public class JoinView extends JFrame implements ActionListener{
 				memberService.add(members[i]);
 			}
 			
-		}else if(e.getSource() == cancelButton) {
+		}else if(e.getSource() == listButton) {
 			
 		}
 		
